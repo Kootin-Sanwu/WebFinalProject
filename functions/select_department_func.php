d8788ef916c1a513937ef41284a5bf71990c30e7
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-08 23:55:11 +0000

    testing

--- functions/select_department_func.php
+++ functions/select_department_func.php
@@ -1,14 +1,16 @@
 <?php
 // include "../settings/connection.php";
 
-$sql = "SELECT * FROM departments";
-$department_result = $conn->query($sql);
+// $sql = "SELECT * FROM departments";
+// $department_result = $conn->query($sql);
 
-if ($department_result->num_rows > 0) {
-    $department_results = mysqli_fetch_all($department_result, MYSQLI_ASSOC);
-    return $department_results;
-} else {
-    echo "Could not fetch roles";
-}
+// if ($department_result->num_rows > 0) {
+//     $department_results = mysqli_fetch_all($department_result, MYSQLI_ASSOC);
+//     return $department_results;
+// } else {
+//     echo "Could not fetch roles";
+// }
 
-$conn->close();
+// $conn->close();
+
+echo "Issue";
