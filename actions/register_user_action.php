93c6f2e3a28aeb8263640d6b1771b532cb57bfd0
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-09 01:44:53 +0000

    testing

--- actions/register_user_action.php
+++ actions/register_user_action.php
@@ -1,39 +1,39 @@
 <?php
 // include "../settings/connection.php";
 
-$first_name = $_POST['firstName'];
-$last_name = $_POST['lastName'];
-$phone_number = $_POST['phoneNumber'];
-$department_ID = $_POST['department_ID'];
-$role_ID = $_POST['role_ID'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+// $first_name = $_POST['firstName'];
+// $last_name = $_POST['lastName'];
+// $phone_number = $_POST['phoneNumber'];
+// $department_ID = $_POST['department_ID'];
+// $role_ID = $_POST['role_ID'];
+// $email = $_POST['email'];
+// $password = $_POST['password'];
 
-$validRolesForDepartment = [
-    1 => [1],
-    2 => [2, 9],
-    3 => [3, 9],
-    4 => [4, 9],
-    5 => [5, 9],
-    6 => [6, 9],
-    7 => [7, 9],
-    8 => [8, 9]
-];
+// $validRolesForDepartment = [
+//     1 => [1],
+//     2 => [2, 9],
+//     3 => [3, 9],
+//     4 => [4, 9],
+//     5 => [5, 9],
+//     6 => [6, 9],
+//     7 => [7, 9],
+//     8 => [8, 9]
+// ];
 
-if (!in_array($role_ID, $validRolesForDepartment[$department_ID])) {
-    header("Location: ../logins/register_view.php?msg=wrong_combination");
-    exit();
-}
+// if (!in_array($role_ID, $validRolesForDepartment[$department_ID])) {
+//     header("Location: ../logins/register_view.php?msg=wrong_combination");
+//     exit();
+// }
 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+// $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO employees (first_name, last_name, phone_number, department_ID, role_ID, email, password) 
-        VALUES ('$first_name', '$last_name', '$phone_number', '$department_ID', '$role_ID', '$email', '$hashed_password')";
+// $sql = "INSERT INTO employees (first_name, last_name, phone_number, department_ID, role_ID, email, password) 
+//         VALUES ('$first_name', '$last_name', '$phone_number', '$department_ID', '$role_ID', '$email', '$hashed_password')";
 
-if ($conn->query($sql) === TRUE) {
-    header("Location: ../logins/login_view.php");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+// if ($conn->query($sql) === TRUE) {
+//     header("Location: ../logins/login_view.php");
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
 
-$conn->close();
+// $conn->close();
