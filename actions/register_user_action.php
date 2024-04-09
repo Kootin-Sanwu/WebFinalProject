d1af778a7d9bd20e72c3d23e8cf1bca6d34bbbeb
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-09 11:43:49 +0000

    testing

--- actions/register_user_action.php
+++ actions/register_user_action.php
@@ -1,54 +1,4 @@
 <?php
-// include "../settings/connection.php";
-
-// $first_name = $_POST['firstName'];
-// $last_name = $_POST['lastName'];
-// $phone_number = $_POST['phoneNumber'];
-// $department_ID = $_POST['department_ID'];
-// $role_ID = $_POST['role_ID'];
-// $email = $_POST['email'];
-// $password = $_POST['password'];
-
-// echo $first_name;
-// echo $last_name;
-// echo $phone_number;
-// echo $department;
-// echo $role;
-// echo $email;
-// echo $password;
-
-// $validRolesForDepartment = [
-//     1 => [1],
-//     2 => [2, 9],
-//     3 => [3, 9],
-//     4 => [4, 9],
-//     5 => [5, 9],
-//     6 => [6, 9],
-//     7 => [7, 9],
-//     8 => [8, 9]
-// ];
-
-// if (!in_array($role_ID, $validRolesForDepartment[$department_ID])) {
-//     header("Location: ../logins/register_view.php?msg=wrong_combination");
-//     exit();
-// }
-
-// $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-// $sql = "INSERT INTO employees (first_name, last_name, phone_number, department_ID, role_ID, email, password) 
-//         VALUES ('$first_name', '$last_name', '$phone_number', '$department_ID', '$role_ID', '$email', '$hashed_password')";
-
-// if ($conn->query($sql) == TRUE) {
-//     echo "No issues";
-//     // header("Location: ../logins/login_view.php");
-// } else {
-//     echo "There is an issue";
-//     echo "Error: " . $sql . "<br>" . $conn->error;
-// }
-
-// $conn->close();
-
-
 include "../settings/connection.php";
 
 $first_name = $_POST['firstName'];
@@ -59,6 +9,13 @@ $role_ID = $_POST['role_ID'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+echo $first_name;
+echo $last_name;
+echo $phone_number;
+echo $department;
+echo $role;
+echo $email;
+echo $password;
 
 $validRolesForDepartment = [
     1 => [1],
@@ -78,35 +35,15 @@ if (!in_array($role_ID, $validRolesForDepartment[$department_ID])) {
 
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-echo $first_name . "<br>";
-echo $last_name . "<br>";
-echo $phone_number . "<br>";
-echo $department_ID . "<br>";
-echo $role_ID . "<br>";
-echo $email . "<br>";
-echo $password . "<br>";
-echo $hashed_password . "<br>";
+$sql = "INSERT INTO employees (first_name, last_name, phone_number, department_ID, role_ID, email, password) 
+        VALUES ('$first_name', '$last_name', '$phone_number', '$department_ID', '$role_ID', '$email', '$hashed_password')";
 
-// Using prepared statements to prevent SQL injection
-$stmt = $conn->prepare("INSERT INTO employees (first_name, last_name, phone_number, department_ID, role_ID, email, password) 
-                        VALUES (?, ?, ?, ?, ?, ?, ?)");
-
-$stmt->bind_param("sssiiss", $first_name, $last_name, $phone_number, $department_ID, $role_ID, $email, $hashed_password);
-
-
-echo $stmt;
-echo $conn;
-
-// Execute the statement
-
-if ($stmt->execute() === TRUE) {
+if ($conn->query($sql) == TRUE) {
     echo "No issues";
     // header("Location: ../logins/login_view.php");
 } else {
     echo "There is an issue";
-    echo "Error: " . $stmt->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// $stmt->close();
-// $conn->close();
-?>
+$conn->close();
--- settings/connection.php
+++ settings/connection.php
@@ -5,7 +5,6 @@ $password =  "";
 $database = "construction";
 
 $conn = new mysqli($servername, $username, $password, $database);
-// echo "Issue";
 
 if ($conn -> connect_error) {
     echo "Issue";
