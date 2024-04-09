86c6502a939d45c4b2abfca7963dcb81a0bc8f29
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-09 10:16:32 +0000

    testing

--- settings/connection.php
+++ settings/connection.php
@@ -7,6 +7,7 @@ $database = "construction";
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn -> connect_error) {
+    echo "Issue";
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
