081d511b95a97adf116f9669a8e3dacc0017476d
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-11 19:46:13 +0000

    testing

--- managements/common_management.php
+++ managements/common_management.php
@@ -1,7 +1,7 @@
 <?php
-// include "../functions/manage_project_func.php";
+include "../functions/manage_project_func.php";
 ?>
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -13,77 +13,6 @@
 </head>
 
 <body>
-    <div class="concept-container">
-        <div class="container-two">
-            <div class="title-container">
-                <h3>PROJECT</h3>
-            </div>
-            <a href="../dashboards/common_dashboard.php">
-                <div class="container-three">
-                    <button name="homeButton">
-                        <h3>DASHBAORD</h3>
-                    </button>
-                </div>
-            </a>
-            <a href="../managements/common_management.php">
-                <div class="container-three">
-                    <button name="manageButton">
-                        <h3>MANAGE PROJECTS</h3>
-                    </button>
-                </div>
-            </a>
-            <a href="../logins/logout_view.php">
-                <div class="container-four">
-                    <button name="settingsButton">
-                        <h3>SETTINGS</h3>
-                    </button>
-                </div>
-            </a>
-        </div>
-        <div class="container-one">
-            <div class="maintitle-container">
-                <h3>PROJECT LIST</h3>
-            </div>
-            <div class="table-container">
-                <table class="styled-table">
-                    <thead>
-                        <tr>
-                            <th>PROJECT NAME</th>
-                            <th>WORKFLOW</th>
-                        </tr>
-                    </thead>
-                    <tbody id="createdChoresTable">
-                        <?php
-                        // displayDepartmentManagementDetails();
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <script src="../javascript/common_dashboard.js" defer></script>
-</body>
-</html> -->
-
-
-<?php
-include "../functions/manage_project_func.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,
-            initial-scale=1.0">
-    <link rel="stylesheet" href="../css/common_management.css">
-    <title>Manage Project</title>
-</head>
-
-<body>
-    <?php
-    // displayCommonManagementDetails();
-    ?>
     <div class="concept-container">
         <div class="container-two">
             <div class="title-container">
