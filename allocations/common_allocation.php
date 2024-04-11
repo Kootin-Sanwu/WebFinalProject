9d59f3ba7a4dd9b2f663f1683b31f29d3ffa7c98
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-11 20:07:18 +0000

    testing

--- allocations/common_allocation.php
+++ allocations/common_allocation.php
@@ -14,7 +14,7 @@ if (isset($_GET['msg']) && $_GET['msg'] == 'edit') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,
             initial-scale=1.0">
-    <link rel="stylesheet" href="../css/admin_allocation.css">
+    <link rel="stylesheet" href="../css/common_allocation.css">
     <title>Manage Projects</title>
 </head>
 
