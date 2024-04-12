bd892e8b1c445c54fafa545ba14cd319e1b46e4b
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-12 03:28:10 +0000

    testing

--- javascript/admin_management.js
+++ javascript/admin_management.js
@@ -1,3 +1,127 @@
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     var homeButton = document.querySelector(".container-three button[name=homeButton]");
+//     var manageButton = document.querySelector(".container-three button[name=manageButton]");
+//     var choreButton = document.querySelector(".container-three button[name=choreButton]");
+//     var requestButton = document.querySelector(".container-three button[name=requestButton]");
+//     var settingsButton = document.querySelector(".container-four button[name=settingsButton]");
+
+//     function submitHover(button) {
+//         button.style.transform = 'translateY(-2px)';
+//         button.style.backgroundColor = '#ef17eb';
+//         button.style.boxShadow = '5px 5px rgb(65, 65, 65)';
+//     };
+
+//     function resetStyles() {
+//         homeButton.style.transform = 'translateY(0px)';
+//         homeButton.style.backgroundColor = '#6f006d';
+//         homeButton.style.boxShadow = '1px 1px rgb(65, 65, 65)';
+
+//         manageButton.style.transform = 'translateY(0px)';
+//         manageButton.style.backgroundColor = '#6f006d';
+//         manageButton.style.boxShadow = '1px 1px rgb(65, 65, 65)';
+
+//         choreButton.style.transform = 'translateY(0px)';
+//         choreButton.style.backgroundColor = '#6f006d';
+//         choreButton.style.boxShadow = '1px 1px rgb(65, 65, 65)';
+
+//         settingsButton.style.transform = 'translateY(0px)';
+//         settingsButton.style.backgroundColor = '#6f006d';
+//         settingsButton.style.boxShadow = '1px 1px rgb(65, 65, 65)';
+
+//         requestButton.style.transform = 'translateY(0px)';
+//         requestButton.style.backgroundColor = '#6f006d';
+//         requestButton.style.boxShadow = '1px 1px rgb(65, 65, 65)';
+//     };
+
+//     function applyAddPressedEffect(button) {
+//         button.style.transform = 'translateY(4px)';
+//         button.style.backgroundColor = "black";
+//         button.style.boxShadow = 'none';
+//     };
+
+//     homeButton.addEventListener('mouseover', function () {
+//         submitHover(this);
+//     });
+
+//     homeButton.addEventListener('mouseout', function () {
+//         resetStyles();
+//     });
+
+//     homeButton.addEventListener('mousedown', function () {
+//         applyAddPressedEffect(this);
+//     });
+
+//     homeButton.addEventListener('mouseup', function () {
+//         submitHover(this);
+//     });
+
+//     manageButton.addEventListener('mouseover', function () {
+//         submitHover(this);
+//     });
+
+//     manageButton.addEventListener('mouseout', function () {
+//         resetStyles();
+//     });
+
+//     manageButton.addEventListener('mousedown', function () {
+//         applyAddPressedEffect(this);
+//     });
+
+//     manageButton.addEventListener('mouseup', function () {
+//         submitHover(this);
+//     });
+
+//     choreButton.addEventListener('mouseover', function () {
+//         submitHover(this);
+//     });
+
+//     choreButton.addEventListener('mouseout', function () {
+//         resetStyles();
+//     });
+
+//     choreButton.addEventListener('mousedown', function () {
+//         applyAddPressedEffect(this);
+//     });
+
+//     choreButton.addEventListener('mouseup', function () {
+//         submitHover(this);
+//     });
+
+//     settingsButton.addEventListener('mouseover', function () {
+//         submitHover(this);
+//     });
+
+//     settingsButton.addEventListener('mouseout', function () {
+//         resetStyles();
+//     });
+
+//     settingsButton.addEventListener('mousedown', function () {
+//         applyAddPressedEffect(this);
+//     });
+
+//     settingsButton.addEventListener('mouseup', function () {
+//         submitHover(this);
+//     });
+
+//     requestButton.addEventListener('mouseover', function () {
+//         submitHover(this);
+//     });
+
+//     requestButton.addEventListener('mouseout', function () {
+//         resetStyles();
+//     });
+
+//     requestButton.addEventListener('mousedown', function () {
+//         applyAddPressedEffect(this);
+//     });
+
+//     requestButton.addEventListener('mouseup', function () {
+//         submitHover(this);
+//     });
+// });
+
+
 document.addEventListener('DOMContentLoaded', function () {
 
     var homeButton = document.querySelector(".container-three button[name=homeButton]");
@@ -41,7 +165,7 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     homeButton.addEventListener('mouseover', function () {
-        submitHover(this);
+        submitHover(homeButton);
     });
 
     homeButton.addEventListener('mouseout', function () {
@@ -49,15 +173,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     homeButton.addEventListener('mousedown', function () {
-        applyAddPressedEffect(this);
+        applyAddPressedEffect(homeButton);
     });
 
     homeButton.addEventListener('mouseup', function () {
-        submitHover(this);
+        submitHover(homeButton);
     });
 
     manageButton.addEventListener('mouseover', function () {
-        submitHover(this);
+        submitHover(manageButton);
     });
 
     manageButton.addEventListener('mouseout', function () {
@@ -65,15 +189,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     manageButton.addEventListener('mousedown', function () {
-        applyAddPressedEffect(this);
+        applyAddPressedEffect(manageButton);
     });
 
     manageButton.addEventListener('mouseup', function () {
-        submitHover(this);
+        submitHover(manageButton);
     });
 
     choreButton.addEventListener('mouseover', function () {
-        submitHover(this);
+        submitHover(choreButton);
     });
 
     choreButton.addEventListener('mouseout', function () {
@@ -81,15 +205,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     choreButton.addEventListener('mousedown', function () {
-        applyAddPressedEffect(this);
+        applyAddPressedEffect(choreButton);
     });
 
     choreButton.addEventListener('mouseup', function () {
-        submitHover(this);
+        submitHover(choreButton);
     });
 
     settingsButton.addEventListener('mouseover', function () {
-        submitHover(this);
+        submitHover(settingsButton);
     });
 
     settingsButton.addEventListener('mouseout', function () {
@@ -97,15 +221,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     settingsButton.addEventListener('mousedown', function () {
-        applyAddPressedEffect(this);
+        applyAddPressedEffect(settingsButton);
     });
 
     settingsButton.addEventListener('mouseup', function () {
-        submitHover(this);
+        submitHover(settingsButton);
     });
 
     requestButton.addEventListener('mouseover', function () {
-        submitHover(this);
+        submitHover(requestButton);
     });
 
     requestButton.addEventListener('mouseout', function () {
@@ -113,10 +237,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     requestButton.addEventListener('mousedown', function () {
-        applyAddPressedEffect(this);
+        applyAddPressedEffect(requestButton);
     });
 
     requestButton.addEventListener('mouseup', function () {
-        submitHover(this);
+        submitHover(requestButton);
     });
 });
