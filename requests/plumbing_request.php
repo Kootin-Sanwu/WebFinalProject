db39b31fa9b91f1d4cd70207260043f74b47906a
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-13 20:09:12 +0000

    testing

--- requests/plumbing_request.php
+++ requests/plumbing_request.php
@@ -93,10 +93,15 @@ include "../settings/core.php";
                     <button onclick="togglePopup()" name="requestButton">REQUEST PROJECT</button>
                 </div>
             </div>
-            <div class="assignchore-popup" id="assignchore-container" style="display: none;">
+            <!-- <div class="assignchore-popup" id="assignchore-container" style="display: none;">
                 <?php
-                include "../administrator/project_request.php";
+                // include "../administrator/project_request.php";
                 ?>
+            </div> -->
+            <div class="assignchore-popup" id="assignchore-container" style="display: none;">
+                <div id="employee-requests">
+                    <!-- AJAX will insert the content here -->
+                </div>
             </div>
             <div class="table-container">
                 <table class="styled-table">
