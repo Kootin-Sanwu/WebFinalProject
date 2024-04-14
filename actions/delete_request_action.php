e7853d345efd141b9282e2738a98eea9bc04a70b
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-14 01:24:18 +0000

    testing

--- actions/delete_request_action.php
+++ actions/delete_request_action.php
@@ -6,6 +6,10 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (isset($_POST['request_ID']) && isset($_POST['employee_ID'])) {
     $request_ID = $_POST['request_ID'];
 
