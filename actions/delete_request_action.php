3a3dd4d3db955865e1ae37d816b022ef57271c45
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-14 01:30:16 +0000

    testing

--- actions/delete_request_action.php
+++ actions/delete_request_action.php
@@ -6,15 +6,15 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 if (isset($_POST['request_ID']) && isset($_POST['employee_ID'])) {
     $request_ID = $_POST['request_ID'];
-
+    
     $sql = "DELETE FROM requests WHERE request_ID = {$request_ID}";
-    header("Location: {$_SERVER['HTTP_REFERER']}");
+    // header("Location: {$_SERVER['HTTP_REFERER']}");
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 } else {
     echo "No request ID or employee ID specified.";
 }
