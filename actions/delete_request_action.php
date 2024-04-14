0c6131aeb485f0ea81018582cbbdf52e43ec2895
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-14 20:22:03 +0000

    testing

--- actions/delete_request_action.php
+++ actions/delete_request_action.php
@@ -11,24 +11,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $employee_ID = $_POST['employee_ID'];
         $close_Value = $_POST['closeButton'];
         $request_ID = $_POST['request_ID'];
+
+        echo $close_Value;
         
-        if ($close_Value == "close") {
-            header("Location: ../directions/close_request_direction.php?msg=close");
-            exit();
-        } else {
-            $sql_delete_request = "DELETE FROM requests WHERE request_ID = ?";
+        // if ($close_Value == "close") {
+        //     header("Location: ../directions/close_request_direction.php?msg=close");
+        //     exit();
+        // } else {
+        //     $sql_delete_request = "DELETE FROM requests WHERE request_ID = ?";
             
-            $stmt_delete_request = $conn->prepare($sql_delete_request);
-            $stmt_delete_request->bind_param("i", $request_ID);
+        //     $stmt_delete_request = $conn->prepare($sql_delete_request);
+        //     $stmt_delete_request->bind_param("i", $request_ID);
         
-            if ($stmt_delete_request->execute()) {
-                header("Location: ../directions/close_request_direction.php?department_ID={$department_ID}");
-            } else {
-                echo "Error deleting request: " . $stmt_delete_request->error;
-            }
+        //     if ($stmt_delete_request->execute()) {
+        //         header("Location: ../directions/close_request_direction.php?department_ID={$department_ID}");
+        //     } else {
+        //         echo "Error deleting request: " . $stmt_delete_request->error;
+        //     }
         
-            $stmt_delete_request->close();
-        }
+        //     $stmt_delete_request->close();
+        // }
 
     } else {
         echo "No request ID, employee ID, department ID, or close value specified.";
