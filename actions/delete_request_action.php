5f20851d51d2fb1f49f138156c5f27169af155a2
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-14 10:51:03 +0000

    testing

--- actions/delete_request_action.php
+++ actions/delete_request_action.php
@@ -12,7 +12,8 @@ if (isset($_POST['request_ID']) && isset($_POST['employee_ID']) && isset($_POST[
     $stmt_delete_request->bind_param("i", $request_ID);
 
     if ($stmt_delete_request->execute()) {
-        header("Location: {$_SERVER['HTTP_REFERER']}");
+        // header("Location: {$_SERVER['HTTP_REFERER']}");
+        header("Location: ../request/plumbing_request.php");
     } else {
         echo "Error deleting request: " . $stmt_delete_request->error;
     }
