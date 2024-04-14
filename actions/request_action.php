a1f94bf8b64b52cf2089c34ab1d1b14142285a9b
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-14 03:24:00 +0000

    testing

--- actions/request_action.php
+++ actions/request_action.php
@@ -1,5 +1,8 @@
 <?php
 include "../settings/connection.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_GET["msg"])) {
     $project_Name = $_POST["project_name"];
