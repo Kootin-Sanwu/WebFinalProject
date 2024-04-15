01bd360e675095bf34b2fa2e2a1f5483d92ea383
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 18:30:28 +0000

    testing

--- functions/assign_project_func.php
+++ functions/assign_project_func.php
@@ -9,12 +9,12 @@
 //         session_start();
 //     }
 
-//     $currentDepartmentID = $_SESSION['department_ID'];
+//     $currentDepartment_ID = $_SESSION['department_ID'];
 
 //     $sql = "SELECT projects.project_ID, projects.project_name, CONCAT(employees.first_name, ' ', employees.last_name) AS assigned_by, projects.status
 //             FROM projects
 //             JOIN employees ON projects.employee_ID = employees.employee_ID
-//             WHERE projects.department_ID = {$currentDepartmentID}";
+//             WHERE projects.department_ID = {$currentDepartment_ID}";
 
 //     $result = $conn->query($sql);
 
@@ -22,15 +22,15 @@
 
 //         while ($row = $result->fetch_assoc()) {
 //             $projectID = $row["project_ID"];
-//             $projectName = $row["project_name"];
-//             $assignedBy = $row["assigned_by"];
+//             $project_Name = $row["project_name"];
+//             $assigned_By = $row["assigned_by"];
 //             $status = $row["status"];
 
 //             echo "<tr>";
-//             echo "<td>{$projectName}</td>";
-//             echo "<td>{$assignedBy}</td>";
-//             echo "<td>{$projectName}</td>";
-//             echo "<td>{$projectName}</td>";
+//             echo "<td>{$project_Name}</td>";
+//             echo "<td>{$assigned_By}</td>";
+//             echo "<td>{$project_Name}</td>";
+//             echo "<td>{$project_Name}</td>";
 //             echo "<td>{$status}</td>";
 
 //             // echo "<td><form class='action-container' action='../allocations/edit_project_action.php' method='post'>";
@@ -47,7 +47,7 @@
 
 
 
-function displayDepartmentAssignments()
+function displayDepartmentProjectDetails()
 {
     include "../settings/connection.php";
 
@@ -56,59 +56,42 @@ function displayDepartmentAssignments()
         session_start();
     }
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    $currentDepartment_ID = $_SESSION['department_ID'];
 
-    $currentDepartmentID = $_SESSION['department_ID'];
-    // echo $currentDepartmentID;
-
-    $sql = "SELECT a.project_ID, p.project_name, CONCAT(e.first_name, ' ', e.last_name) AS assigned_by, p.workflow, p.status
+    $sql = "SELECT a.project_ID, p.project_name, CONCAT(e.first_name, ' ', e.last_name) AS assigned_by, p.workflow, p.status, a.begin_date, a.end_date
             FROM assignments a
             JOIN projects p ON a.project_ID = p.project_ID
             JOIN employees e ON p.employee_ID = e.employee_ID
-            WHERE a.department_ID = {$currentDepartmentID}";
-
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);        
+            WHERE a.department_ID = {$currentDepartment_ID}";
 
     $result = $conn->query($sql);
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
-    echo $result->num_rows;
-
     if ($result->num_rows > 0) {
 
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
-
         while ($row = $result->fetch_assoc()) {
+            $project_Name = $row["project_name"];
+            $assigned_By = $row["assigned_by"];
+            $begin_Date = $row["begin_date"];
             $projectID = $row["project_ID"];
-            $projectName = $row["project_name"];
-            $assignedBy = $row["assigned_by"];
+            $end_Date = $row["end_date"];
             $workflow = $row["workflow"];
             $status = $row["status"];
 
             echo "<tr>";
-            echo "<td>{$projectID}</td>";
-            echo "<td>{$assignedBy}</td>";
-            echo "<td>{$workflow}</td>";
-            echo "<td>{$projectName}</td>";
-            echo "<td>{$status}</td>";
+            echo "<td>{$project_Name}</td>";
+            echo "<td>{$assigned_By}</td>";
+            echo "<td>{$begin_Date}</td>";
+            echo "<td>{$end_Date}</td>";
 
             echo "</tr>";
         }
     } else {
-        echo "0 results";
+        echo "No Assignments";
     }
 
     $conn->close();
 }
+
 
 
 function displayProjectDetails()
@@ -127,15 +110,15 @@ function displayProjectDetails()
 
         while ($row = $result->fetch_assoc()) {
             $projectID = $row["project_ID"];
-            $projectName = $row["project_name"];
-            $assignedBy = $row["assigned_by"];
+            $project_Name = $row["project_name"];
+            $assigned_By = $row["assigned_by"];
             $status = $row["status"];
 
             echo "<tr>";
-            echo "<td>{$projectName}</td>";
-            echo "<td>{$assignedBy}</td>";
-            echo "<td>{$projectName}</td>";
-            echo "<td>{$projectName}</td>";
+            echo "<td>{$project_Name}</td>";
+            echo "<td>{$assigned_By}</td>";
+            echo "<td>{$project_Name}</td>";
+            echo "<td>{$project_Name}</td>";
 
             echo "<td><form class='action-container' action='../actions/delete_project_action.php' method='post'>";
             echo "<input type='hidden' name='project_ID' value='{$projectID}'>";
@@ -172,22 +155,22 @@ function displayAllAssignments()
         
         while ($row = $result->fetch_assoc()) {
             $assignmentID = $row["assignment_ID"];
-            $projectName = $row["project_name"];
-            $assignedBy = $row["assigned_by"];
+            $project_Name = $row["project_name"];
+            $assigned_By = $row["assigned_by"];
             $departmentName = $row["department_name"];
-            $beginDate = $row["begin_date"];
-            $endDate = $row["end_date"];
+            $begin_Date = $row["begin_date"];
+            $end_Date = $row["end_date"];
             
                 ini_set('display_errors', 1);
                 ini_set('display_startup_errors', 1);
                 error_reporting(E_ALL);
 
             echo "<tr>";
-            echo "<td>{$projectName}</td>";
-            echo "<td>{$assignedBy}</td>";
+            echo "<td>{$project_Name}</td>";
+            echo "<td>{$assigned_By}</td>";
             echo "<td>{$departmentName}</td>";
-            echo "<td>{$beginDate}</td>";
-            echo "<td>{$endDate}</td>";
+            echo "<td>{$begin_Date}</td>";
+            echo "<td>{$end_Date}</td>";
 
             echo "<td><form class='action-container' action='../actions/delete_assignment_action.php' method='post'>";
             echo "<input type='hidden' name='assignment_ID' value='{$assignmentID}'>";
@@ -215,14 +198,14 @@ function displayDepartmentAssignmentDetails()
     }
 
     // Get the current department_ID from the session
-    $currentDepartmentID = $_SESSION['department_ID'];
+    $currentDepartment_ID = $_SESSION['department_ID'];
 
     // SQL query to fetch assignments of the current department
     $sql = "SELECT assignment.assignment_ID, projects.project_ID, projects.project_name, CONCAT(employees.first_name, ' ', employees.last_name) AS assigned_by, assignment.begin_date, assignment.end_date
             FROM assignment
             JOIN projects ON assignment.project_ID = projects.project_ID
             JOIN employees ON projects.employee_ID = employees.employee_ID
-            WHERE assignment.department_ID = {$currentDepartmentID}";
+            WHERE assignment.department_ID = {$currentDepartment_ID}";
 
     $result = $conn->query($sql);
 
@@ -232,16 +215,16 @@ function displayDepartmentAssignmentDetails()
         while ($row = $result->fetch_assoc()) {
             $assignment_ID = $row["assignment_ID"];
             $project_ID = $row["project_ID"];
-            $projectName = $row["project_name"];
-            $assignedBy = $row["assigned_by"];
-            $beginDate = $row["begin_date"];
-            $endDate = $row["end_date"];
+            $project_Name = $row["project_name"];
+            $assigned_By = $row["assigned_by"];
+            $begin_Date = $row["begin_date"];
+            $end_Date = $row["end_date"];
 
             echo "<tr>";
-            echo "<td>{$projectName}</td>";
-            echo "<td>{$assignedBy}</td>";
-            echo "<td>{$beginDate}</td>";
-            echo "<td>{$endDate}</td>";
+            echo "<td>{$project_Name}</td>";
+            echo "<td>{$assigned_By}</td>";
+            echo "<td>{$begin_Date}</td>";
+            echo "<td>{$end_Date}</td>";
 
             echo "</tr>";
         }
@@ -261,14 +244,14 @@ function displayRecentAssignmentDetails()
     }
 
     // Get the current department_ID from the session
-    $currentDepartmentID = $_SESSION['department_ID'];
+    $currentDepartment_ID = $_SESSION['department_ID'];
 
     // SQL query to fetch assignments of the current department
     $sql = "SELECT assignment.assignment_ID, projects.project_ID, projects.project_name, CONCAT(employees.first_name, ' ', employees.last_name) AS assigned_by, assignment.begin_date, assignment.end_date
             FROM assignment
             JOIN projects ON assignment.project_ID = projects.project_ID
             JOIN employees ON projects.employee_ID = employees.employee_ID
-            WHERE assignment.department_ID = {$currentDepartmentID}";
+            WHERE assignment.department_ID = {$currentDepartment_ID}";
 
     $result = $conn->query($sql);
 
@@ -278,16 +261,16 @@ function displayRecentAssignmentDetails()
         while ($row = $result->fetch_assoc()) {
             $assignment_ID = $row["assignment_ID"];
             $project_ID = $row["project_ID"];
-            $projectName = $row["project_name"];
-            $assignedBy = $row["assigned_by"];
-            $beginDate = $row["begin_date"];
-            $endDate = $row["end_date"];
+            $project_Name = $row["project_name"];
+            $assigned_By = $row["assigned_by"];
+            $begin_Date = $row["begin_date"];
+            $end_Date = $row["end_date"];
 
             echo '<div class="assigned-container">';
-            echo "<p>ASSIGNMENT: {$projectName}</p>";
-            echo "<p>ASSIGNED BY: {$assignedBy}</p>";
-            echo "<p>START DATE:  {$beginDate}</p>";
-            echo '<a href="../requests/allocation_redirect.php?msg=allocate&department_ID=' . $currentDepartmentID . '"><button name="generalAssignmentButton">VIEW DETAILS</button></a>';
+            echo "<p>ASSIGNMENT: {$project_Name}</p>";
+            echo "<p>ASSIGNED BY: {$assigned_By}</p>";
+            echo "<p>START DATE:  {$begin_Date}</p>";
+            echo '<a href="../requests/allocation_redirect.php?msg=allocate&department_ID=' . $currentDepartment_ID . '"><button name="generalAssignmentButton">VIEW DETAILS</button></a>';
             echo '</div>';
 
             echo "</tr>";
@@ -307,7 +290,7 @@ function displayAdminRecentAssignmentDetails()
     }
 
     // Get the current department_ID from the session
-    $currentDepartmentID = $_SESSION['department_ID'];
+    $currentDepartment_ID = $_SESSION['department_ID'];
 
     // SQL query to fetch assignments of the current department
     // $sql = "SELECT * FROM assignment ORDER BY assignment_ID DESC LIMIT 4";
@@ -329,15 +312,15 @@ function displayAdminRecentAssignmentDetails()
         while ($row = $result->fetch_assoc()) {
             $assignment_ID = $row["assignment_ID"];
             $project_ID = $row["project_ID"];
-            $projectName = $row["project_name"];
-            $assignedBy = $row["assigned_by"];
-            $beginDate = $row["begin_date"];
-            $endDate = $row["end_date"];
+            $project_Name = $row["project_name"];
+            $assigned_By = $row["assigned_by"];
+            $begin_Date = $row["begin_date"];
+            $end_Date = $row["end_date"];
 
             echo '<div class="assigned-container">';
-            echo "<p>ASSIGNMENT: {$projectName}</p>";
-            echo "<p>ASSIGNED BY: {$assignedBy}</p>";
-            echo "<p>START DATE:  {$beginDate}</p>";
+            echo "<p>ASSIGNMENT: {$project_Name}</p>";
+            echo "<p>ASSIGNED BY: {$assigned_By}</p>";
+            echo "<p>START DATE:  {$begin_Date}</p>";
             echo '<a href="../allocations/admin_allocation.php"><button name="generalAssignmentButton">VIEW DETAILS</button></a>';
             echo '</div>';
 
