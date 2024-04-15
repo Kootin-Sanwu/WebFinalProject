e41c4514aba19ef54b0a915f96afd388dd2e2973
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 15:56:15 +0000

    testing

--- actions/delete_project_action.php
+++ actions/delete_project_action.php
@@ -3,31 +3,37 @@ include "../settings/connection.php";
 
 // Check if project_ID is set in POST request
 if (isset($_POST['project_ID'])) {
+    $close_Value = $_POST['closeButton'];
     $project_ID = $_POST['project_ID'];
 
-    // Check if the project_ID exists in the assignment table
-    $checkSql = "SELECT * FROM assignments WHERE project_ID = ?";
-
-    $stmt = $conn->prepare($checkSql);
-    $stmt->bind_param("i", $project_ID);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        // echo "Cannot delete project as it is assigned to a department";
-        header("Location: ../managements/admin_management.php?msg=cannot delete");
+    if ($close_Value == "close") {
+        header("Location: ../directions/close_delete_direction.php?department_ID={$department_ID}");
         exit();
     } else {
-        // SQL query to delete project from projects table
-        $sql = "DELETE FROM projects WHERE project_ID = ?";
+        // Check if the project_ID exists in the assignment table
+        $checkSql = "SELECT * FROM assignments WHERE project_ID = ?";
 
-        $stmt = $conn->prepare($sql);
+        $stmt = $conn->prepare($checkSql);
         $stmt->bind_param("i", $project_ID);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        if ($stmt->execute()) {
-            header("Location: ../managements/admin_management.php");
+        if ($result->num_rows > 0) {
+            // echo "Cannot delete project as it is assigned to a department";
+            header("Location: ../managements/admin_management.php?msg=cannot delete");
+            exit();
         } else {
-            echo "Error deleting project: " . $stmt->error;
+            // SQL query to delete project from projects table
+            $sql = "DELETE FROM projects WHERE project_ID = ?";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("i", $project_ID);
+
+            if ($stmt->execute()) {
+                header("Location: ../managements/admin_management.php");
+            } else {
+                echo "Error deleting project: " . $stmt->error;
+            }
         }
     }
 } else {
