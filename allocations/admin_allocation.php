ef1572060adb28ade7cde0c151229ab50204fb5a
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 13:09:55 +0000

    testing

--- allocations/admin_allocation.php
+++ allocations/admin_allocation.php
@@ -106,6 +106,10 @@ if (isset($_GET['msg']) && $_GET['msg'] == 'cannot delete') {
                     </thead>
                     <tbody id="createdChoresTable">
                         <?php
+                        
+                        ini_set('display_errors', 1);
+                        ini_set('display_startup_errors', 1);
+                        error_reporting(E_ALL);
                         displayAllAssignments();
                         ?>
                     </tbody>
