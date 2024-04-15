0ab5befe42a3b7e0cdc9a4d2cf21fa8e52f260a7
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 14:16:05 +0000

    testing

--- actions/request_action.php
+++ actions/request_action.php
@@ -1,9 +1,9 @@
 <?php
 include "../settings/connection.php";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_GET["msg"])) {
     $message = $_GET["msg"];
