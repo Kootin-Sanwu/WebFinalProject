c49c33a7e7d4ac908ecccfa8d2e93f7c85e42793
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 11:29:30 +0000

    testing

--- directions/close_edit_direction.php
+++ directions/close_edit_direction.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-if (isset($_GET['department_ID']) && isset($_GET['msg']) && $_GET['msg'] == 'cannot_edit') {
+if (isset($_GET['msg']) && $_GET['msg'] == 'cannot_edit') {
     $department_ID = $_GET['department_ID'];
 
     switch ($department_ID) {
@@ -36,6 +36,35 @@ if (isset($_GET['department_ID']) && isset($_GET['msg']) && $_GET['msg'] == 'can
             exit();
     }
 } else if (isset($_GET['msg']) && $_GET['msg'] == 'close'){
-    header("Location: {$_SERVER['HTTP_REFERER']}");
-    exit();
+    $department_ID = $_GET['department_ID'];
+
+    switch ($department_ID) {
+        case 8:
+            header("Location: ../requests/welding_request.php");
+            exit();
+        case 7:
+            header("Location: ../requests/surveying_request.php");
+            exit();
+        case 6:
+            header("Location: ../requests/roofing_request.php");
+            exit();
+        case 5:
+            header("Location: ../requests/carpentry_request.php");
+            exit();
+        case 4:
+            header("Location: ../requests/concretery_request.php");
+            exit();
+        case 3:
+            header("Location: ../requests/electrical_request.php");
+            exit();
+        case 2:
+            header("Location: ../requests/plumbing_request.php");
+            exit();
+        case 1:
+            header("Location: ../managements/admin_management.php");
+            exit();
+        default:
+            header("Location: ../logins/login_view.php?msg=Not Logged In");
+            exit();
+    }
 }
