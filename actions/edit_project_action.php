17adfe1c78b7eb04c70f11aa8b49772d97ad2440
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 16:33:24 +0000

    testing

--- actions/edit_project_action.php
+++ actions/edit_project_action.php
@@ -8,24 +8,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the values from the form
     $project_name = $_POST['project_name'];
     $employee_ID = $_POST['employee_ID'];
+    $close_Value = $_POST['closeButton'];
     $project_ID = $_POST['project_ID'];
     $begin_date = $_POST['begin_date'];
     $end_date = $_POST['end_date'];
 
-    // SQL query to update the project request using prepared statements
-    $sql = "UPDATE projects SET project_name = ?, begin_date = ?, end_date = ?, status = 'APPROVED' WHERE project_ID = ?";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssi", $project_name, $begin_date, $end_date, $project_ID);
-
-    if ($stmt->execute()) {
+    if ($close_Value == "close") {
         header("Location: ../managements/admin_management.php");
+        exit();
     } else {
-        echo "Error updating record: " . $stmt->error;
+        $sql = "UPDATE projects SET project_name = ?, begin_date = ?, end_date = ?, status = 'APPROVED' WHERE project_ID = ?";
+    
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sssi", $project_name, $begin_date, $end_date, $project_ID);
+    
+        if ($stmt->execute()) {
+            header("Location: ../managements/admin_management.php");
+        } else {
+            echo "Error updating record: " . $stmt->error;
+        }
+    
+        $stmt->close();
+        $conn->close();
     }
-
-    $stmt->close();
-    $conn->close();
 } else {
     echo "Form not submitted.";
 }
