7f8e045f3adf5e0dfb9766654f14b2bc1d8e14dc
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 11:39:55 +0000

    testing

--- directions/close_edit_direction.php
+++ directions/close_edit_direction.php
@@ -8,28 +8,28 @@ if (isset($_GET['msg']) && $_GET['msg'] == 'cannot_edit') {
 
     switch ($department_ID) {
         case 8:
-            header("Location: ../requests/welding_request.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../requests/welding_request.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         case 7:
-            header("Location: ../requests/surveying_request.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../requests/surveying_request.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         case 6:
-            header("Location: ../requests/roofing_request.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../requests/roofing_request.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         case 5:
-            header("Location: ../requests/carpentry_request.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../requests/carpentry_request.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         case 4:
-            header("Location: ../requests/concretery_request.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../requests/concretery_request.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         case 3:
-            header("Location: ../requests/electrical_request.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../requests/electrical_request.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         case 2:
-            header("Location: ../requests/plumbing_request.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../requests/plumbing_request.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         case 1:
-            header("Location: ../managements/admin_management.php?msg=cannot_edit&department_ID{$department_ID}");
+            header("Location: ../managements/admin_management.php?msg=cannot_edit&department_ID={$department_ID}");
             exit();
         default:
             header("Location: ../logins/login_view.php?msg=Not Logged In");
