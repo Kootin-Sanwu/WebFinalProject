3afa4f2c988c893a44fba1e1e34b71639536cb77
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-04-15 11:31:58 +0000

    testing

--- actions/edit_request_action.php
+++ actions/edit_request_action.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: ../directions/close_edit_direction.php?msg=cannot_edit&department_ID={$department_ID}");
         exit();
     } else if (isset($_GET['msg']) && $_GET['msg'] == 'cannot_edit') {
-        header("Location: ../directions/close_edit_constraint.php?msg=cannot_edit&department_ID{$department_ID}");
+        header("Location: ../directions/close_edit_constraint.php?msg=cannot_edit&department_ID={$department_ID}");
         exit();
     } else {
 
